eddd105a90d7c32cfe2efdb441095837f005333d
carojas <carojas@inteliglobe.com>
2017-10-20 14:56:52 -0500

    agregando base de datos de produccion

--- web_tagger/settings/produccion.py
+++ web_tagger/settings/produccion.py
@@ -3,3 +3,16 @@ from base import *
 
 print("Running Produccion Conf")
 
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'taggersys',
+        'USER': 'crojas',
+        'PASSWORD': '123qweasd',
+        'HOST': '1234qwerasdfzxcv=)',
+        'PORT': '5432',
+    }
+}
+
